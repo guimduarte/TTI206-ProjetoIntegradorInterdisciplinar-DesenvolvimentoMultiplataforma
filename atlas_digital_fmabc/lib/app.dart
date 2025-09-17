b59bc3287456f1046a5e772a15afc8ea72200996
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-17 18:07:58 -0300

    feat(theme): aplicação do tema de cores da FMABC ao Material Design

--- atlas_digital_fmabc/lib/app.dart
+++ atlas_digital_fmabc/lib/app.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/material.dart';
+import "src/theme/theme.dart";
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
+  /// Root Widget da aplicação.
   @override
   Widget build(BuildContext context) {
+    /// Tema do app.
+    final materialTheme = MaterialTheme(ThemeData.light().textTheme);
+
     return MaterialApp(
-      title: 'Atlas Digital FMABC',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Atlas Digital FMABC'),
+      title: 'Atlas Digital FMABC', // título do app
+      theme: materialTheme.light(), // tema claro
+      darkTheme: materialTheme.dark(), // tema escuro
+      home: const MyHomePage(title: 'Atlas Digital FMABC'), // página inicial
     );
   }
 }
@@ -105,6 +93,11 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            ElevatedButton(onPressed: () {}, child: Text("Teste")),
+            FilledButton(onPressed: () {}, child: Text("Teste")),
+            FilledButton.tonal(onPressed: () {}, child: Text("Teste")),
+            OutlinedButton(onPressed: () {}, child: Text("Teste")),
+            TextButton(onPressed: () {}, child: Text("Teste")),
           ],
         ),
       ),
