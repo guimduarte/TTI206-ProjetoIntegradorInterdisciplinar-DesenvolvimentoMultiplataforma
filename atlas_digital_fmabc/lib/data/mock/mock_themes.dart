84cf4176a83d583d0419a27ef52077d9e7b6338d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-16 11:41:02 -0300

    feat(mock): exemplos de temas

---
+++ atlas_digital_fmabc/lib/data/mock/mock_themes.dart
@@ -0,0 +1,23 @@
+import 'package:atlas_digital_fmabc/models/groups/theme_model.dart';
+
+/// Exemplo de temas.
+final List<ThemeModel> mockThemes = [
+  ThemeModel(name: "Epitélio", quantSlides: 10),
+  ThemeModel(name: "Tecido Conjuntivo", quantSlides: 8),
+  ThemeModel(name: "Músculo", quantSlides: 12),
+  ThemeModel(name: "Cartilagem e Osso", quantSlides: 15),
+  ThemeModel(name: "Tecido Nervoso", quantSlides: 9),
+  ThemeModel(name: "Sangue Periférico", quantSlides: 11),
+  ThemeModel(name: "Hematopoiese", quantSlides: 7),
+  ThemeModel(name: "Sistema Cardiovascular", quantSlides: 6),
+  ThemeModel(name: "Sistema Linfoide", quantSlides: 5),
+  ThemeModel(name: "Pele", quantSlides: 14),
+  ThemeModel(name: "Glândulas Exócrinas", quantSlides: 4),
+  ThemeModel(name: "Glândulas Endócrinas", quantSlides: 3),
+  ThemeModel(name: "Trato Gastrointestinal", quantSlides: 13),
+  ThemeModel(name: "Fígado e Vesícula Biliar", quantSlides: 10),
+  ThemeModel(name: "Sistema Urinário", quantSlides: 8),
+  ThemeModel(name: "Sistema Respiratório", quantSlides: 9),
+  ThemeModel(name: "Sistema Reprodutor Feminino", quantSlides: 12),
+  ThemeModel(name: "Sistema Reprodutor Masculino", quantSlides: 11),
+];
