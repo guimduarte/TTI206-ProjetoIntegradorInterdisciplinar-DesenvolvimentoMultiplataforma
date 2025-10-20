eec05fe1c97803d8cd461be3792945c703f3143b
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-10-20 02:25:17 -0300

    feat(database) adicionando e lendo imagens do banco de dados

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,31 @@
 from contextlib import asynccontextmanager
 from os import stat
-from fastapi import FastAPI, HTTPException, Request, Response, UploadFile, status
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Path, Request, Response, UploadFile, status
 import json
 from dotenv import dotenv_values
+import tempfile
+from src.db.image import ImageDB
 from pymongo import MongoClient
 from src.image import generate_image, decompress_file
 
 config = dotenv_values(".env")
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     # on startup
-#     app.mongodb_client = MongoClient(config["ATLAS_URI"])
-#     app.database = app.mongodb_client[config["DB_NAME"]]
-#     yield
-#     # on shutdown
-#     app.mongodb_client.close()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on startup
+    app.mongodb_client = MongoClient(config["DB_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    yield
+    # on shutdown
+    app.mongodb_client.close()
     
-# app = FastAPI(lifespan=lifespan)
-app = FastAPI()
-    
+app = FastAPI(lifespan=lifespan)    
+
+def getDatabase(collection_name : str):
+    return app.database[collection_name]
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -34,9 +39,9 @@ async def upload_image(file: UploadFile):
     return {"message" : "sucesso"}
 
 
-@app.get("/image")
-async def get_image(request : Request):
-    request_json = await request.json()
-    body = request_json["body"]
-    print(body)
-    
+@app.get("/image/{image_name}/{x}/{y}/{z}")
+async def get_image(image_name : Annotated[str, Path()], x : Annotated[int, Path()],
+                    y : Annotated[int, Path()], z : Annotated[int, Path()]):
+    imagedb = ImageDB(image_name, getDatabase(ImageDB.collection_name))
+    file = imagedb.get_image(f"{x}/{y}/{z}")
+    return Response(file)
