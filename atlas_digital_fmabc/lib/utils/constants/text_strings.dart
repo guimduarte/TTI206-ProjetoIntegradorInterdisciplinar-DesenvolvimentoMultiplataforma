47bf627be3c1c5e66d56440ad6b4d9e53f48473e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-31 10:43:39 -0300

    feat(constants/string): criação de arquivo para recursos de strings

---
+++ atlas_digital_fmabc/lib/utils/constants/text_strings.dart
@@ -0,0 +1,15 @@
+/// Constantes de strings de texto usadas na aplicação.
+class KTexts {
+  // -- AppName
+  static const String appName = "Atlas Digital FMABC";
+  static const String appFullName = "Atlas Digital de Biologia Tecidual";
+  static const String appNameAppBar =
+      "Atlas Digital de Biologia Tecidual | FMABC";
+
+  // -- University
+  static const String universityName = "Faculdade de Medicina do ABC";
+  static const String universityAcronym = "FMABC";
+  static const String universityEmail = "fmabc@fmabc.br";
+  static const String universityWebsite = "https://fmabc.br";
+  static const String universityTelephone = "(11) 4993-5400";
+}
