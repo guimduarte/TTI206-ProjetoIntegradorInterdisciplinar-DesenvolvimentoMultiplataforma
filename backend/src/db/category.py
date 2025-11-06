5a3e1c34b43ada93522a14e67a4ff041def52cea
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-11-06 09:57:46 -0300

    feat(category) adicionar crud de category

---
+++ backend/src/db/category.py
@@ -0,0 +1,61 @@
+from typing import List, Optional
+
+
+class CategoryDB:
+    collection_name = "category"
+
+    def __init__(self, db):
+        self.db = db
+
+    def get_categories(self):
+        return self.db.find()
+
+    def create_category(self, category_name : str, images : List[str]):
+        item = {
+            "category_name" : category_name,
+            "images" : images
+        }
+        self.db.insert_one(item)
+        print("categoria criada")
+
+    def update_category(self, category_name : str, new_name : Optional[str], new_images : Optional[List[str]]):
+        if new_name is None:
+            new_name = category_name
+        new_item = {}
+        if new_images is None:
+            new_item = {
+                "$set": {
+                    "category_name" : new_name
+                }
+            }
+        else:
+            new_item = {
+                "$set": {
+                    "category_name" : new_name,
+                    "images" : new_images
+                }
+            }
+        self.db.update_one({"category_name" : category_name}, new_item)
+        print("categoria atualizada")
+
+    def delete_category(self, category_name : str):
+        self.db.delete_one({"category_name" : category_name})
+        print("categoria deletada")
+
+    def get_category_images(self, category_name : str):
+        return self.db.aggregate({
+            "$match" : {
+                "category_name" : category_name
+            },
+            "$unwind" : "$images",
+            "$lookup" : {
+                "from" : "images",
+                "localField" : "image_info",
+                "foreignField" : "image_name",
+                "as" : "image_info"
+            },
+            "$project" : {
+                "_id" : 0,
+                "image_info.image_name" : 1
+            }
+        })
