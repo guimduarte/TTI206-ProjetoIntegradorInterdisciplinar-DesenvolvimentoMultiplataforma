75b823c476b18f64d29e98de4456f328ae798141
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-11-08 15:03:25 -0300

    fix(login) arrumar login no front

--- atlas_digital_fmabc/lib/services/auth_service.dart
+++ atlas_digital_fmabc/lib/services/auth_service.dart
@@ -3,12 +3,11 @@ import 'package:http/http.dart' as http;
 
 class AuthService {
   static const String _baseUrl = 'http://localhost:8000';
-  
 
   Future<String?> register({
     required String name,
-    required String email, 
-    required String password
+    required String email,
+    required String password,
   }) async {
     final url = Uri.parse('$_baseUrl/register');
     final response = await http.post(
@@ -17,7 +16,7 @@ class AuthService {
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        'name':name,
+        'name': name,
         'email': email,
         'password': password,
       }),
@@ -27,14 +26,14 @@ class AuthService {
       return 'Registration successful!';
     } else {
       final responseBody = jsonDecode(response.body);
-      return responseBody['message'] ?? 'An unknown error occurred during registration.';
+      return responseBody['message'] ??
+          'An unknown error occurred during registration.';
     }
   }
 
-
-  Future<String?> login({
-    required String email, 
-    required String password
+  Future<Map<String, dynamic>> login({
+    required String email,
+    required String password,
   }) async {
     final url = Uri.parse('$_baseUrl/login');
     final response = await http.post(
@@ -42,20 +41,18 @@ class AuthService {
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: jsonEncode(<String, String>{
-        'email': email,
-        'password': password,
-      }),
+      body: jsonEncode(<String, String>{'email': email, 'password': password}),
     );
 
     if (response.statusCode == 200) {
       final responseBody = jsonDecode(response.body);
-      String token = responseBody['token'] ?? 'MOCK_TOKEN_123'; 
-      print('Login successful. Received token: $token'); 
-      return null; 
+      String token = responseBody['token'] ?? 'MOCK_TOKEN_123';
+      bool isAdmin = responseBody['is_admin'] ?? false;
+      print('Login successful. Received token: $token $isAdmin');
+      return {"token": token, "is_admin": isAdmin};
     } else {
       final responseBody = jsonDecode(response.body);
-      return responseBody['message'] ?? 'Login failed. Please try again.';
+      return {"error" : responseBody["detail"]};
     }
   }
 }
