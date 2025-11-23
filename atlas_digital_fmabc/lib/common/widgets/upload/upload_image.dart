d9ce389e0757f705c857f5d4de7f9c8e79a3ccd1
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-11-22 21:33:49 -0300

    fix(upload) adicionar padrão posix ao .tar

--- atlas_digital_fmabc/lib/common/widgets/upload/upload_image.dart
+++ atlas_digital_fmabc/lib/common/widgets/upload/upload_image.dart
@@ -38,12 +38,13 @@ class _UploadImageState extends State<UploadImage> {
     final outputFileName = p.absolute(p.dirname(directoryPath), "temp.tar");
     final outputFile = File(outputFileName).openWrite();
     final List<TarEntry> entries = [];
+    var pathContext = p.Context(style: p.Style.posix);
     await for (final entity in directory.list(recursive: true)) {
       if (entity is File) {
         entries.add(
           TarEntry.data(
             TarHeader(
-              name: p.relative(entity.path, from: directoryPath),
+              name: pathContext.relative(entity.path, from: directoryPath),
               mode: int.parse('644', radix: 8),
             ),
             File(entity.path).readAsBytesSync(),
